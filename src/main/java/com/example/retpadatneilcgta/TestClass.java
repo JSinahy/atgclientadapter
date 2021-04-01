b75362d5568e6343f433a3fe00042fa1337e4ca1
José Sinahy <jslaraestudillo@gmail.com>
2021-04-01 07:39:28 -0600

    Primer envio de libreria

---
+++ src/main/java/com/example/retpadatneilcgta/TestClass.java
@@ -0,0 +1,9 @@
+package com.example.retpadatneilcgta;
+
+import android.util.Log;
+
+public class TestClass {
+    public static void Greetings(){
+        Log.wtf("SALUDOS", "HOLA MI NEGRO");
+    }
+}
